35c9600da20d25aad94f5a1780da4f2f322e7c60
Horitashingo <horita-singo-cx@ynu.jp>
2016-03-15 22:02:11 +0900

    Update CalcVisitor

--- src/route/main/CalcVisitor.java
+++ src/route/main/CalcVisitor.java
@@ -1,23 +1,74 @@
 package route.main;
 
+import konoha.script.KonohaChecker.False;
+import konoha.script.KonohaChecker.True;
+
 public abstract class CalcVisitor {
 	public abstract Object visit(Add node);
 
 	public abstract Object visit(Mul node);
-	
+
 	public abstract Object visit(Equals node);
-	
+
 	public abstract Object visit(NotEquals node);
-	
+
 	public abstract Object visit(GreaterThan node);
-	
+
 	public abstract Object visit(GreaterThanEquals node);
-	
+
 	public abstract Object visit(LessThan node);
-	
+
 	public abstract Object visit(LessThanEquals node);
-	
-	public abstract Object visit(Int node);
-	
+
+	public abstract Object visit(Intager node);
+
 	public abstract Object visit(Source node);
+
+	public abstract Object visit(Vardecl node);
+
+	public abstract Object visit(In node);
+
+	public abstract Object visit(Out node);
+
+	public abstract Object visit(Funcdecl node);
+
+	public abstract Object visit(Name node);
+
+	public abstract Object visit(Arglist node);
+
+	public abstract Object visit(ArgristII node);
+
+	public abstract Object visit(Returnlist node);
+
+	public abstract Object visit(Return node);
+
+	public abstract Object visit(OthwiseRet node);
+
+	public abstract Object visit(Returncase node);
+
+	public abstract Object visit(Where node);
+
+	public abstract Object visit(Declist node);
+
+	public abstract Object visit(Elemtype node);
+
+	public abstract Object visit(FuncCall node);
+
+	public abstract Object visit(And node);
+
+	public abstract Object visit(Or node);
+
+	public abstract Object visit(Unop node);
+
+	public abstract Object visit(Minus node);
+
+	public abstract Object visit(Not node);
+
+	public abstract Object visit(In node);
+
+	public abstract Object visit(Nil node);
+
+	public abstract Object visit(False node);
+
+	public abstract Object visit(True node);
 }
